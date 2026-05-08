7879dbdd028ecb85b888ea1d38780258fec1b3e1
krishnendu <ppkrishnendu113@gmail.com>
2026-05-08 10:55:11 +0530

    ARROBJ

---
+++ ArrObj/index.js
@@ -0,0 +1,65 @@
+
+    /*const users = [
+        "kpp","avn","rrj"*/
+    ;
+    /*console.log(users)
+    console.log(users[users.length-1])*/
+
+/*users.push(" spp")
+for(let i=0;i<users.length;i++){
+    console.log(users[i])
+}
+users.pop("kpp")
+for(let i=0;i<users.length -1;i++){
+    console.log(users[i])
+}
+*/
+
+
+/*let arr=[1,2,3,4,5];
+for
+    (let i=0;i<arr.length;i++)
+    console.log(arr[i])
+//create object
+    const person ={
+        name:"KPP",age:21,dep:"CSD"};
+
+        //display value
+        console.log("Name",person.name);
+        console.log("Age",person.age);
+        console.log("dep",person.dep);
+
+        // or display whole object
+        console.log(person);*/
+
+
+       /* const num =[1,2,3,4,5,6];
+        const sqr=num.map( num=>num*num);
+        console.log(sqr);
+
+        const evenum=num.filter(num=>num%2==0);
+       console.log(evenum);*/
+
+     /*  const num =[1,2,3,4,5,6];
+       const multy=num.map( num=>num*2);
+       console.log(multy);
+
+         const salary =[10000,20300];
+       const bonus=salary.map( salary=>salary+1000);
+       console.log(bonus);
+
+       
+ const name =["achu","akku","tuttu","kannan"];
+       const cap=name.map( name=>name.toUpperCase());
+       console.log(cap);
+
+ const num =[1,2,3,44,5,16];
+const great=num.filter(num=>num>10);
+       console.log(great);*/
+
+const add =([x,y]) =>x+y;
+console.log(add([5,4]));
+
+console 
+
+    
