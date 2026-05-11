a621a8daf3f52f6cb5906ef1ec783c6a1f5e1efe
krishnendu <ppkrishnendu113@gmail.com>
2026-05-11 09:21:39 +0530

    first commit

--- usestate/my-react-app/src/App.jsx
+++ usestate/my-react-app/src/App.jsx
@@ -2,25 +2,15 @@ import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [input1,SetInput1] = useState('')
-  const [input2,SetInput2] = useState('')
-  const [text,SetText]=useState('Hello')
+  const f =["aa","bb","cc"];
+  
 
   return (
     <>
-    <div className='text'>
-      <p>{text}</p>
-      <button onClick={()=>SetText("Welcome")}>Click</button><br />
-    </div>
-    <br /><br />
-    <form>
+    
+  {  f.map((i,index)=> (<h1 key ={index}> {i}</h1>))}
 
-      <label>Name: </label><input type="text" onChange={(e)=>SetInput1(e.target.value)}></input>
-      <label>Email: </label><input type="email" onChange={(e)=>SetInput2(e.target.value)}></input>
-      <h4>Name: {input1}</h4>
-      <h4>Email: {input2}</h4>
 
-    </form>
     
     </>
   ) 
